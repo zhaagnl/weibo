fc466be375e51dd4fc298cdda3249d3afa420957
MrZhang <zslemail@qq.com>
2026-07-21 15:35:27 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,9 @@ Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('
 // 对提交过来的token和Email数据进行配对，正确的话更新密码
 Route::post('password/reset', 'PasswordController@reset')->name('password.update');
 
+// 微博的创建和删除
+Route::resource('statuses', 'StatusesController',['only' => ['store', 'destroy']]);
+
 
 // Auth::routes();
 
